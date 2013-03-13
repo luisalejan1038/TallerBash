a18443da5b812e503a36e3b5e0c39570b377e8d9
unknown <luis.silva@VISUALSALE2.mind.com.co>
2013-03-13 16:47:40 -0500

    TallerBash

--- ScriptBackUps/BackUp1.sh
+++ ScriptBackUps/BackUp1.sh
@@ -7,14 +7,14 @@ mail_for_errors="luis.silva@mind.com.co"
 
 #Scripts begins here 
 #logfile=$(Pb5.txt)                         #redireccionar la stderr de los programas que usamos a lo largo del script, para poder recopilar los errores y enviarlos por mail 
-inc_dir="${backup_dir}/.zip"
+inc_dir="${backup_dir}/.tar"
 inc_file="${inc_dir}/incremental.info"
 [ -d "$backup_dir" ] || mkdir -p "$backup_dir"
 [ -d "$inc_dir" ] || mkdir -p "$inc_dir"
 
 #borrar backups vencidos 
-find $backup_dir -name "back-$(hostname)
--*" -ctime + $backup_days -a -type f -exec rm -f {} \; 
+#find $backup_dir -name "back-$(hostname)
+#-*" -ctime + $backup_days -a -type f -exec rm -f {} \; 
 
 # si encuentro .lock es porque fallo el full backup, borro inc_file para que se haga de nuevo
 [ -f "${inc_file}.lock" -a -f "$inc_file" ] && rm -f "$inc_file"
@@ -22,20 +22,20 @@ find $backup_dir -name "back-$(hostname)
 #setup full or incremental
 if [ $(date +%u) -eq 7 -o ! -f "$inc_file" ]; then 
    #backup-full
-   back_name="${backup_dir}/backup-$(hostname)-full-$(date +%Y-%m-%d).zip.gz"
+   back_name="${backup_dir}/backup-$(hostname)-full-$(date +%Y-%m-%d).tar.gz"
    #borro inc_file para que el backup sea full 
    [ -f "$inc_file" ] && rm -f "$inc_file"
    touch "${inc_file}.lock"
 else 
    #backup-inc
-   backup_name="${backup_dir}/backup-$(hostname)-inc-$(date +%A).zip.gz"   
+   backup_name="${backup_dir}/backup-$(hostname)-inc-$(date +%A).tar.gz"   
    cp $inc_file ${inc_file}.tmp
    inc_file=${inc_file}.tmp
 fi
 
 
 #finally, do the backup 
-tarme "$inc_file" "$backup_name"
+tar cvf "$inc_file" "$backup_name"
 
 
 
